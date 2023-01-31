b89f158462af5c6d1d82d2f6ead6c4594d7cd435
Mailine Nguyen <64129348+MailineN@users.noreply.github.com>
2023-01-31 10:23:02 +0100

    last attempt

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0',
+    host: 'acdc.demo.insee.io',
     port: 3000,
   },
   plugins: [react()],
