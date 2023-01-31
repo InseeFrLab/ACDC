57a008e4e18133b1adda21dc0c11dab2ea0bbf46
Mailine Nguyen <64129348+MailineN@users.noreply.github.com>
2023-01-31 08:51:14 +0100

    🌓

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
-    watch: {
-      usePolling: true,
-    },
-    open: false,
-    host: true,
-    strictPort: true,
+    host: '0.0.0.0',
     port: 3000,
   },
+  plugins: [react()],
 });
