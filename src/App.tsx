5bfc85bda66c119e812cb5381be69f3a7d349ee7
Mailine Nguyen <64129348+MailineN@users.noreply.github.com>
2023-02-23 17:18:33 +0100

    translation: fix translation issues in create collection event form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import './App.css';
+import { useTranslation } from 'react-i18next';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { fr } from 'date-fns/locale';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { LocalizationProvider } from '@mui/x-date-pickers';
@@ -10,7 +12,7 @@ const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
+    <LocalizationProvider dateAdapter={AdapterDateFns} locale={fr}>
       <QueryClientProvider client={queryClient}>
         <Root
           sx={{
